3bbd270f1167d5b995472330854ba14ca96fbbb2
csgsky <674143140@qq.com>
2017-09-28 00:32:38 +0800

    Rx 和 Rect 结合

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
-import { 
+import Rx from 'rxjs'
+import {
   BrowserRouter as Router,
   Route,
   Link
@@ -12,6 +13,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      result: 0,
+      testArr: [1, 2, 3, 4, 4, 5, 6, 7]
+    }
+  }
+  componentDidMount() {
+    let input = this.refs.titleInput
+    Rx.Observable
+      .fromEvent(input, 'keypress')
+      .filter(e => e.keyCode === 13)
+      .map(() => input.value)
+      .filter(v => v.length > 0)
+      .subscribe((value) => {
+        console.log(value)
+      })
+
+    // Rx.Observable
+    //   .from(this.state.testArr)
+    //   .distinct()
+    //   .scan((origin, next) => origin + next, 0)
+    //   .subscribe(it => {
+    //     console.log(it)
+    //     this.setState({result: it})
+    //   }
+    // )
+  }
+
+
   render() {
     return (
       <Router>
@@ -23,7 +55,11 @@ class App extends Component {
           <p className="App-intro">
             react、 redux、 react-router、 redux-action、 redux-observable、rxjs.
           </p>
-          <button className="App-count">{this.props.count}</button>
+          <button className="App-count">{this.state.result}</button>
+          <br />
+          <input type="text" ref="titleInput"/>
+          <br /><br /><br />
+          <button className="App-add">{this.props.count}</button>
           <br />
           <button className="App-add" onClick={this.props.add}>add</button>
           <br />
